646664110c1e7bf2772273403224b4ff6341c0b7
Subhrajitbw <subhrajitbhowmik80@gmail.com>
2025-11-26 16:14:34 +0530

    refactor: update configuration files and remove deprecated ones

--- medusa-config.js
+++ medusa-config.js
@@ -5,7 +5,14 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
+        databaseDriverOptions: {
+         ssl: { rejectUnauthorized: false },
+         connection: { ssl: { rejectUnauthorized: false } }
+    },
+
+    // Redis (set REDIS_URL in production; falls back to local)
+    redisUrl: process.env.REDIS_URL || "redis://localhost:6379",
+
     http: {
       storeCors: process.env.STORE_CORS,
       adminCors: process.env.ADMIN_CORS,
@@ -15,25 +22,25 @@ module.exports = defineConfig({
     }
   },
   modules: [
-    //     {
-    //   resolve: "@medusajs/medusa/file",
-    //   options: {
-    //     providers: [
-    //       {
-    //         resolve: "@medusajs/medusa/file-s3",
-    //         id: "s3",
-    //         options: {
-    //           file_url: process.env.FILE_BASE_URL,
-    //           access_key_id: process.env.S3_ACCESS_KEY_ID,
-    //           secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-    //           region: process.env.S3_REGION,
-    //           bucket: process.env.S3_BUCKET,
-    //           endpoint: process.env.S3_ENDPOINT,
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
+        {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.FILE_BASE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: "./src/modules/product-media",
     },
@@ -77,7 +84,7 @@ module.exports = defineConfig({
         apiKey: process.env.ALGOLIA_API_KEY,
         productIndexName: process.env.ALGOLIA_PRODUCT_INDEX_NAME,
       },
-    },  
+    },
     {
       resolve: "./src/modules/meilisearch",
       options: {
@@ -116,3 +123,6 @@ module.exports = defineConfig({
       },
     },
 })
+
+
+
